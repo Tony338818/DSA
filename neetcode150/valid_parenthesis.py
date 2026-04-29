4bd375fc980093091215084192939bdfada6b110
Tony <chidubemigwe449@gmail.com>
2026-04-29 15:20:47 +0100

    add: valid parenthesis

---
+++ neetcode150/valid_parenthesis.py
@@ -0,0 +1,46 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+"""
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        if len(s) % 2 != 0:
+            return False
+        
+        param = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        
+        stack = []
+        
+        for c in s:
+            if c in '({[':
+                stack.append(c)
+                
+            else:
+                if not stack:
+                    return False
+                
+                top = stack.pop()
+                
+                if param[c] != top:
+                    return False
+        return len(stack) == 0
+    
+s = "(]"
+sol = Solution()
+result = sol.isValid(s=s)
+print(result)
