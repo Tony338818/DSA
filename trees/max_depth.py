e0df38e371dc2b2b26036fb1ad8b5fbce5318559
Tony <chidubemigwe449@gmail.com>
2026-05-13 23:57:41 +0100

    add: learning trees

---
+++ trees/max_depth.py
@@ -0,0 +1,16 @@
+"""
+Given the root of a tree:
+find the maximum depth
+"""
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class Solution:
+    def max_depth(self, root):
+        """
+        
+        """
